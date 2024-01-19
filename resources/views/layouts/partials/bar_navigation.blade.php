2159e5a5dc8534f1bdbf3c3d3d12f77cdc6cd234
syifa <syindiaris@gmail.com>
2024-01-19 14:01:50 +0700

    profil

---
+++ resources/views/layouts/partials/bar_navigation.blade.php
@@ -0,0 +1,19 @@
+
+<nav class="bg-gradient-to-r  from-cyan-500 to-cyan-800">
+  <div class="mx-auto max-w-7xl px-5 sm:px-6 lg:px-8 h-24">
+  <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
+        <div class="flex flex-shrink-0 items-center mr-40 mt-2  ">
+          <img class="h-20 w-auto pr-2" src="/img/lg.png" alt="">
+          <h1 class="bg-clip-text text-transparent bg-gradient-to-r from-cyan-900 to-cyan-700 font-mono font-bold text-3xl">SIKAMU</h1>
+        </div>
+        <div class="hidden sm:ml-6 sm:block mt-8 ">
+          <div class="d-flex space-x-4 ml-80 pl-60 ">
+            <a href="#" class="text-cyan-100 hover:text-cyan-400  text-sm font-medium">BERANDA</a>
+            <a href="#" class="text-cyan-100 hover:text-cyan-400  text-sm font-medium">BANTUAN</a>
+            <a href="#" class="text-cyan-100 hover:text-cyan-400  text-sm font-medium">JADWAL RONDA</a>
+          </div>
+        </div>
+      </div>
+   
+  </div>
+</nav>   
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,6 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/logout', [AuthController::class, 'logout'])->name('logoutProses');
     });;
 });
+Route::get('/greeting', function () {
+    return view('home.profil.profil');
+});
