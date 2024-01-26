a321c40ec0cdae17dac0a563d4b23de47ece69fb
RizalS <87477682+RizalSFN@users.noreply.github.com>
2024-01-26 08:44:22 +0700

    jadwal proses

--- app/Http/Controllers/RondaController.php
+++ app/Http/Controllers/RondaController.php
@@ -18,17 +18,51 @@ class RondaController extends Controller
         return view('page.ronda.index', ["title" => "ronda", 'data' => $data]);
     }
 
+    public function jadwal()
+    {
+        $datas = RondaData::all();
+        $data = $datas[0];
+        $hasil = [];
+        $senin = array_push($hasil, explode('-', $data->senin));
+        $selasa = array_push($hasil, explode('-', $data->selasa));
+        // dd($hasil);
+        $rabu = explode('-', $data->rabu);
+        $kamis = explode('-', $data->kamis);
+        $jumat = explode('-', $data->senin);
+        $sabtu = explode('-', $data->sabtu);
+        $minggu = explode('-', $data->minggu);
+
+        return view('page.ronda.jadwal', compact('hasil'));
+    }
+
     public function acak()
     {
         $data = Warga::all(['nama']);
         // dd($data[0]);
+        $count = count($data);
+        $hasil = ceil($count / 7);
 
         $jadwal = [[], [], [], [], [], [], []];
-        // dd(count($data));
+        // dd($hasil);
+        // dd(random_int(random_int(1, 3), random_int(5, 7)));
         foreach ($data as $d) {
-            array_push($jadwal[random_int(0, 6)], $d->nama);
+            if (count($jadwal[0]) === $hasil) {
+                array_push($jadwal[random_int(1, 6)], $d->nama);
+            } else if (count($jadwal[1]) === $hasil) {
+                array_push($jadwal[array_rand([0, 2, 3, 4, 5, 6])], $d->nama);
+            } else if (count($jadwal[2]) === $hasil) {
+                array_push($jadwal[array_rand([0, 1, 3, 4, 5, 6])], $d->nama);
+            } else if (count($jadwal[3]) === $hasil) {
+                array_push($jadwal[array_rand([0, 1, 2, 4, 5, 6])], $d->nama);
+            } else if (count($jadwal[4]) === $hasil) {
+                array_push($jadwal[array_rand([0, 1, 2, 3, 5, 6])], $d->nama);
+            } else if (count($jadwal[5]) === $hasil) {
+                array_push($jadwal[array_rand([0, 1, 2, 3, 4, 6])], $d->nama);
+            } else {
+                array_push($jadwal[random_int(0, 6)], $d->nama);
+            }
         }
-
+        dd($jadwal);
         return view('page.ronda.jadwal', compact('jadwal'));
     }
 
--- resources/views/page/ronda/jadwal.blade.php
+++ resources/views/page/ronda/jadwal.blade.php
@@ -1,35 +1,41 @@
 @extends('layouts.main')
 
 @section('content')
-    <table class="border-2 border-black">
-        <tr class="border-2 border-black">
-            <th class="p-3">Senin</th>
-            <th class="p-3">Selasa</th>
-            <th class="p-3">Rabu</th>
-            <th class="p-3">Kamis</th>
-            <th class="p-3">Jumat</th>
-            <th class="p-3">Sabtu</th>
-            <th class="p-3">Minggu</th>
-            <th class="p-3">Dari</th>
-            <th class="p-3">Sampai</th>
-        </tr>
-        <tr class="border-2 border-black">
-            @foreach ($jadwal as $j)
-                <td>
-                    <ol>
-                        @foreach ($j as $user)
-                            <li>{{ $user }}</li>
-                        @endforeach
-                    </ol>
-                </td>
-            @endforeach
-            <td>7 Januari 2023</td>
-            <td>7 Januari 2024</td>
-        </tr>
-    </table>
+    <form action="" method="POST">
+        @foreach ($hasil as $h)
+            <div class="mt-4">
+                <ul class="list-disc">
+                    @foreach ($h as $d)
+                        <li>{{ $d }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+        <div>
+            <label>Senin</label>
+            <div>
+                <input type="text" disabled name="senin1" value=>
+                <input type="text" disabled name="senin2">
+                <input type="text" disabled name="senin3">
+                <input type="text" disabled name="senin4">
+                <input type="text" disabled name="senin5">
+            </div>
+        </div>
+        <div>
+            <label>Selasa</label>
+            <div>
+                <input type="text" disabled name="selasa1">
+                <input type="text" disabled name="selasa2">
+                <input type="text" disabled name="selasa3">
+                <input type="text" disabled name="selasa4">
+                <input type="text" disabled name="selasa5">
+            </div>
+        </div>
+    </form>
     <div class="mt-8">
-        <a href="#" class="border-2 p-3 bg-red-500 text-white hover:bg-red-600">Pilih manual</a>
         <a href="{{ route('ronda.jadwal.acak') }}" class="border-2 p-3 bg-blue-500 text-white hover:bg-blue-600">Pilih
             otomatis</a>
+        <a href="{{ route('ronda.jadwal.acak') }}"
+            class="border-2 p-3 bg-green-500 text-white hover:bg-green-600">Simpan</a>
     </div>
 @endsection
